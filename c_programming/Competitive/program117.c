bd518b0b2d1903f35708c5f32397c31532401c7e
Tanaya khaire <tanayakhaire01@gmail.com>
2026-06-29 18:03:38 +0530

    Add files via upload

---
+++ c_programming/Competitive/program117.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i =0,j =0;
+    
+    for(i=1;i<= iRow ;i++)
+    {
+        for(j=0;j<=iCol; j++)
+        {
+            if(i%2 ==0 )
+            {
+              printf("%c\t",'a'+j);
+
+            }
+            else
+            {
+              printf("%c\t",'A'+j);
+
+            }
+            
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1=0,iValue2=0;
+    
+    printf("Enter the number of rows and columns :");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+
+}
