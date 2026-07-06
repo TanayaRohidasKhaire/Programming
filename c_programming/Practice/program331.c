28da62ca2dd0c17f31f50887b05847283753ab99
Tanaya khaire <tanayakhaire01@gmail.com>
2026-07-06 21:51:30 +0530

    Add files via upload

---
+++ c_programming/Practice/program331.c
@@ -0,0 +1,22 @@
+
+//Accept no from user and toggal 11th bit
+
+# include<stdio.h>
+typedef unsigned int UINT;
+
+int main()
+{
+    UINT iNo =0;
+    UINT iMask=0; 
+
+    printf("Enter number \n");
+    scanf("%d",&iNo);
+
+    iMask = 0x400;//11
+
+    iNo =iNo^ iMask;
+
+    printf("Updated number %d\n",iNo);
+
+    return 0;
+}
---
+++ c_programming/Practice/program333.c
@@ -0,0 +1,27 @@
+
+//Accept no from user and accept position toggal that position bit
+
+# include<stdio.h>
+typedef unsigned int UINT;
+
+int main()
+{
+    UINT iNo =0;
+    UINT iMask=0x1;
+    UINT iPos=0; 
+
+    printf("Enter number \n");
+    scanf("%d",&iNo);
+
+    printf("Enter position \n");
+    scanf("%d",&iPos);
+
+    iMask = iMask << (iPos -1);
+
+
+    iNo =iNo ^ iMask;
+
+    printf("Updated number %d\n",iNo);
+
+    return 0;
+}
